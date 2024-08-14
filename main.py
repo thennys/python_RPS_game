40034e348b4e5fc9bb34e28162912d75b14d8db8
thennys <thennyswhyte@gmail.com>
2024-08-14 10:39:30 -1200

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import random
+import sys
+
+
+class RPS:
+    def __init__(self):
+        print('welcome to Rock🪨, Paper📜, Scissors✂️')
+
+        self.moves = {'rock': '🪨🪨🪨', 'paper': '📜📜📜', 'scissors': '✂️✂️✂️'}
+
+        self.valid_moves = list(self.moves.keys())
+
+        def play_game():
+            pass
+
+        def display_moves():
+            ...
+            
+        def check_moives():
+            ...
