5925ebbc06b99fc217f11a5d0c63e58b0d9fbc38
thennys <thennyswhyte@gmail.com>
2024-08-14 10:56:07 -1200

    play game method implemented

--- main.py
+++ main.py
@@ -10,11 +10,24 @@ class RPS:
 
         self.valid_moves = list(self.moves.keys())
 
-        def play_game():
-            pass
+    def play_game(self):
+        user_move = input("Rock, Paper, Scissors").lower()
+        
+        if user_move == 'exit':
+            print("Thanks for playing, see you soon")
+            sys.exit()
 
-        def display_moves():
-            ...
-            
-        def check_moives():
-            ...
+        if user_move not in self.valid_moves:
+            print('invalid moves...')
+            return self.play_game()
+
+        ai_move = random.choice(self.valid_moves)
+
+        self.display_moves(user_move, ai_move)
+        self.check_moves(user_move, ai_move)
+
+    def display_moves(self,):
+        ...
+        
+    def check_moves(self,):
+        ...
